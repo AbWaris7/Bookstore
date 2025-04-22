70ca69fc19a478bbf0ab117a933b7df08c3910a4
abdulwariskhan <hellokhan.ch@gmail.com>
2025-04-22 22:45:42 +0200

    Initial commit

---
+++ src/test/java/com/khan/code/Books/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khan.code.Books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
